c0bdf14616669e8f4519b43e5ebdeddf2e513171
nikigor <n.gorbadey@gmail.com>
2019-03-22 02:44:29 +0300

    Stable version
    Signed-off-by: nikigor <n.gorbadey@gmail.com>

--- RM.h
+++ RM.h
@@ -16,8 +16,9 @@ uint8_t *sum(uint8_t *vector_1, uint8_t *vector_2, int size);
 /*
  * Decoder functions
  */
+uint8_t *RPA_RM(uint8_t m, uint8_t r, float *L, float theta, uint8_t N_max);
 coset_t calc_cosets_vectors(uint8_t *z_0, uint8_t m, uint8_t** Z);
-uint8_t * prod(uint8_t* vector_1, uint8_t* vector_2,int  size);
+uint8_t *prod(uint8_t* vector_1, uint8_t* vector_2,int  size);
 float *LLR_proj(float *L, coset_t coset, int m);
 uint8_t *FD_R_2(float *L, uint8_t m, uint8_t** Z);
 float *divide(float *L, float c, int n);
