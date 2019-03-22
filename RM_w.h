19633c51abe67199d1e41210f7e975367d57593f
nikigor <n.gorbadey@gmail.com>
2019-03-22 08:25:17 +0300

    10000 words test
    Signed-off-by: nikigor <n.gorbadey@gmail.com>

---
+++ RM_w.h
@@ -0,0 +1,7 @@
+#ifndef RM_W_H_
+#define RM_W_H_
+#include <stdint.h>
+
+extern uint8_t rm_words[9991][256];
+
+#endif //RM_W_H_
---
+++ RM_words.h
@@ -0,0 +1,7 @@
+#ifndef RM_WORDS_H_
+#define RM_WORDS_H_
+#include <stdint.h>
+
+extern float rm_codewords[9991][256];
+
+#endif //RM_WORDS_H_
